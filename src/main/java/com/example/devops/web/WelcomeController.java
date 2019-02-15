cd149a492e79e4755f5d9a7f64c2ecc3e04859cf
Antonio Gómez Extremera <antonio.gomez@langaesco.com>
2019-02-15 12:41:15 +0100

    Sonar Integration changes

--- src/main/java/com/example/devops/web/WelcomeController.java
+++ src/main/java/com/example/devops/web/WelcomeController.java
@@ -4,12 +4,13 @@ import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 public class WelcomeController {
 	private final Logger logger = LoggerFactory.getLogger(this.getClass());
 
-	@RequestMapping("/")
+	@RequestMapping(value="/", method = RequestMethod.GET)
 	public String welcome(Model model) {
 		logger.info("Processing index request");
 		model.addAttribute("course", "DevOps");
